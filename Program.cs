7925ff931a9713127c95efb543093d76ed8ad505
Lucas Vogel <lucas.vogel@endpointsystems.com>
2017-05-17 15:15:06 -0400

    initial add

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionAtRest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dm = new FigaroDataManager();
+            dm.InsertRecordFromUrl("http://bdbxml.net/sitemap.xml");
+            dm.Dispose();
+        }
+    }
+}
